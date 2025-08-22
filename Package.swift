86363393c996243ecc93fd60b15fb086c2c92012
isac <isac9305@gaeasoft.co.kr>
2025-08-22 10:51:20 +0900

    - 의존성 패키지 업데이트

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["IsacCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/IsacShin/IsacFoundation.git", from: "1.0.0")
+        .package(url: "https://github.com/IsacShin/IsacFoundation.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
